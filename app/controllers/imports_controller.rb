e21b895c7b2dc49b3ae55920bae5840ee827b069
Fong Fan <fong@siimpl.io>
2017-06-27 00:17:29 -0700

    Creates functionality to upload the csv

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,4 +1,23 @@
 class ImportsController < ApplicationController
   def index
+    @imports = Import.all
+  end
+
+  def new
+    @import = Import.new
+  end
+
+  def create
+    @import = Import.new(import_params)
+
+    @import.save
+
+    redirect_to imports_path
+  end
+
+  private
+
+  def import_params
+    params.require(:import).permit(:original_file)
   end
 end
